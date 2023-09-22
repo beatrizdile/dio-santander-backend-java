498c8a2af4c5c401092ff4c0f4eeaff173f445ce
Beatriz Dile <bedos-sa@student.42sp.org.br>
2023-09-22 19:29:31 -0300

    types

---
+++ dio-3-java-types/src/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte age = 18;
+        short year = 2023;
+        int zipCode = 21070442;
+        long cpf = 98123782L;
+        float pi = 3.14F;
+        double wage = 2745.50;
+
+        short shortNum = 1;
+        int intNum = shortNum;
+        short shortNum2 = (short) intNum;
+
+        // Need to use 'final' to be a const char and use uppercase letters
+        final double PI = 3.14;
+
+        // How to declare strings
+        String myName = "Beatriz Dile";
+
+    }
+}
