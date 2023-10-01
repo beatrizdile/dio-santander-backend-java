553994753f43ee7ca5924217e9141987b6b50e3d
Beatriz Dile <bedos-sa@student.42sp.org.br>
2023-10-01 12:33:19 -0300

    anatomy

---
+++ dio-1-2-java-anatomy/src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String [] args) {
+        int year = 2023;
+        System.out.println(year);
+        year = 2004;
+        System.out.println(year);
+
+        final String BR = "Brazil";
+        double PI = 3.14;
+        int YEAR_2000 = 2000;
+        System.out.println(BR);
+
+        int _numero = 0;
+        int $numero = 0;
+    }
+}
